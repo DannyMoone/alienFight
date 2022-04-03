0efb21a8affde91ac8074f1b71ecfd7f527a197b
Daniel Murillo <88091925+DannyMoone@users.noreply.github.com>
2022-04-03 01:28:56 +0000

    Logo screen size auto

--- js/game.js
+++ js/game.js
@@ -1,12 +1,30 @@
 /* global Phaser*/
 
+//Cpyright (c) 2022 Turing Games All rights reserved
+//
+// Created by: Daniel Murillo
+// Created on: Apr 2022
+// This is Paser3 configuration file
+
+
+//* Game scene*/
 const config = {
   type: Phaser.AUTO, 
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
-}
+  physics: {
+    default: 'arcade',
+    arcade:  {
+      debug: true
+    }
+  },
 
+  backgroundColor: 0x5f6e7a,
+    scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+    }
+}
 
 const game = new Phaser.Game(config)
 console.log(game)
